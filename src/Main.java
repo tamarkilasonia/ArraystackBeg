402bf94988042de6e4e1547ca4f386ac43619059
Killas <t.kilasonia2006@gmail.com>
2025-02-20 17:46:34 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the 
 public class Main {
     public static void main(String[] args) {
