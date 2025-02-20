66c3696f3f406fb0e755975435d440a5c06287bc
Killas <t.kilasonia2006@gmail.com>
2025-02-20 17:46:12 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the 
 public class Main {
     public static void main(String[] args) {
         Stack<Integer> stack = new ArrayStack<>(5); // Stack with capacity 5
